a5f94d60664bedc814134eb2fb7364201f7c750a
Michele Lacchia <michelelacchia@gmail.com>
2020-08-07 18:44:41 +0200

    Merge with desktop configuration

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 
 if type "xrandr"; then
-    for m in $(xrandr --query | grep "connected" | cut -d' ' -f1); do
+    for m in $(xrandr --query | grep " connected" | cut -d' ' -f1); do
         MONITOR=$m polybar main &
     done
 else
